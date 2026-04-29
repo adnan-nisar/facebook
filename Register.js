8d086419513c76ed00ef4b64d60f69d4fd4dca2e
Adnan-Nisar <adnannisar782@gmail.com>
2026-04-29 13:06:56 +0500

    changed file name

--- Register.js
+++ Register.js
@@ -1,6 +1,6 @@
 
 function toggleText(e) {
-    e.preventDefault(); // stop page reload
+    e.preventDefault();
 
     let moreText = document.getElementById("moreText");
 
@@ -12,3 +12,31 @@ function toggleText(e) {
         e.target.innerText = "Learn more.";
     }
 }
+
+function validateSignup(e) {
+    const requiredInputs = document.querySelectorAll('.inputs input, .passwords input');
+    for (let input of requiredInputs) {
+        if (!input.value.trim()) {
+            alert('All fields are required');
+            return false;
+        }
+    }
+
+    const gender = document.querySelector('input[name="sex"]:checked');
+    if (!gender) {
+        alert('All fields are required');
+        return false;
+    }
+
+    return true;
+}
+
+const signupBtn = document.querySelector('.signupbtn');
+if (signupBtn) {
+    signupBtn.addEventListener('click', function (e) {
+        if (!validateSignup()) {
+            e.preventDefault();
+        }
+    });
+}
+
